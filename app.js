bfba7c4e234646d3dee16c43418ea7b563441df1
JMorDev <juanmoralesp19@gmail.com>
2023-09-23 00:56:14 -0300

    Agrego method override

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 
 // Configuración del motor de vistas y archivos estáticos
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+// Configuración de middlewares en el orden correcto
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home')

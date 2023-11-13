df17c3be6b9fd549c88e7b8bb6e23e1ca2f5e0d3
JMorDev <juanmoralesp19@gmail.com>
2023-11-13 20:08:00 -0300

    test

--- frontend/whattodo/src/components/NavBar/NavBar.js
+++ frontend/whattodo/src/components/NavBar/NavBar.js
@@ -1,34 +1,36 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faIdCard, faEye, faKey, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import logo from '../../assets/img/logowhat.png'
+import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
 import './NavBar.css';
 
 function NavBar() {
   return (
     <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary navwhat">
-    <div class="container-fluid nav">
-      <a class="navbar-brand" href="#"><img src={logo} alt="descripción" className='logo' /></a>
-      <button class="navbar-toggler navega" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle cuenta" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Cuenta
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Perfil</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Crea una Experiencia</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid nav">
+          <Link to="/" className="navbar-brand"><img src={logo} alt="descripción" className='logo' /></Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+                <Link to="/cuenta" className="nav-link dropdown-toggle cuenta" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Cuenta
+                </Link>
+                <ul className="dropdown-menu">
+                  <li><Link to="/action" className="dropdown-item">Action</Link></li>
+                  <li><Link to="/crea-experiencia" className="dropdown-item">Crea una Experiencia</Link></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><Link to="/something-else" className="dropdown-item">Something else here</Link></li>
+                </ul>
+              </li>
             </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-    </nav>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 }

c18651905fefccf96ae45d483426810e3454f596
JMorDev <juanmoralesp19@gmail.com>
2023-12-05 19:17:11 -0300

    Otro fondo

--- frontend/whattodo/src/components/HeroSection/HeroSection.js
+++ frontend/whattodo/src/components/HeroSection/HeroSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Hero.css'; // Asegúrate de crear un archivo Hero.css para tus estilos
-import backgroundImage from '../../assets/img/hero-background.jpg'; // Reemplaza con tu imagen de fondo
+import backgroundvideo from '../../assets/video.mp4'; // Reemplaza con tu imagen de fondo
 import SearchBar from '../SearchBar/SearchBar';
 
 const Hero = () => {
@@ -11,12 +11,17 @@ const Hero = () => {
 
   return (
     <div className="hero-cont">
-    <div className="hero" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <video autoPlay loop muted className="background-video">
+      <source src={backgroundvideo} type="video/mp4" />
+    </video>
+    <div className="hero-content">
       <h1 className="hero-title">Descubre tu próxima aventura</h1>
       <SearchBar onSearch={handleSearch} />
     </div>
-    </div>
+  </div>
+  
   );
 };
 
 export default Hero;
+{/* <div className="hero" style={{ backgroundImage: `url(${backgroundImage})` }}> */}
